54c94196f39acc407b153fae8099d6e1588aefa4
Mathic <rizbalt@gmail.com>
2013-07-17 17:53:55 -0400

    First commit
    Creating RPGame

---
+++ RPCharacter/IHeroBuilderOrigin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCharacter
+{
+    interface IHeroBuilderOrigin
+    {
+        IHeroBuilderOrigin From(Hero.RaceType raceType);
+        IHeroBuilderOrigin As(Hero.ClassType classType);
+        IHeroBuilderStats Lvl(int level);
+    }
+}
---
+++ RPCharacter/Monster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCharacter
+{
+    class Monster : ICharacter
+    {
+        private Guid _id;
+        private string _name;
+        private string _dialog;
+
+        public Monster(Guid id, string name, string dialog)
+        {
+            _id = id;
+            _name = name;
+            _dialog = dialog;
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Dialog
+        {
+            get { return _dialog; }
+            set { _dialog = value; }
+        }
+    }
+}
